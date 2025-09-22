a80ca400ed5a01cd31e543809c407a39e8df785d
Vinicius Damiani <vinidamiani03@hotmail.com>
2025-09-22 20:09:51 -0300

    feat: Adicionado novos execícios avaliativos

---
+++ Avaliativos/Lista09/fipe/fipe/src/main/java/com/example/fipe/Service/FipeService.java
@@ -0,0 +1,5 @@
+package Avaliativos.Lista09.fipe.fipe.src.main.java.com.example.fipe.Service;
+
+public class FipeService {
+    
+}
---
+++ Avaliativos/Lista09/fipe/fipe/src/main/java/com/example/fipe/model/FipeResponse.java
@@ -0,0 +1,5 @@
+package Avaliativos.Lista09.fipe.fipe.src.main.java.com.example.fipe.model;
+
+public class FipeResponse {
+    
+}
